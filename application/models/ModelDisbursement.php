649fbf710e84128d9081540dfde42f4caa605ec8
FASHAH\Fashah Darullah <fashahd@gmail.com>
2018-04-12 15:31:02 +0700

    first commit

---
+++ application/models/ModelDisbursement.php
@@ -0,0 +1,117 @@
+<?php
+	class ModelDisbursement extends CI_Model {
+
+		function cek_amount($company_id,$amount){
+			$sql 	= "SELECT company_balance FROM `pouch_mastercompanyaccount` WHERE company_id = ?";
+			$query	= $this->db->query($sql,array($company_id));
+			if($query->num_rows()>0){
+				$row = $query->row();
+				$company_balance = $row->company_balance;
+			}else{
+				$company_balance = 0;
+			}
+			if($amount > $company_balance){
+				return "not_enough";
+			}else{
+				return "enough";
+			}
+		}
+
+		function cek_company_id($company_id,$user_id){
+			$sql 	= "SELECT * FROM `pouch_masteremployeecredential` WHERE userID = ? and company_id = ?";
+			$query	= $this->db->query($sql,array($user_id,$company_id));
+			if($query->num_rows()>0){
+				return "registed";
+			}else{
+				return "unregisted";
+			}
+		}
+
+        function create($data){
+			list($external_id,$amount,$bank_code,$account_name,$account_number,$description,$user_id,$company_id)=$data;
+			$disburse_id = $this->disburse_id($company_id);
+			$now = date("Y-m-d H:i:s");
+			$inserdata = array(
+				"disburse_id"=>$disburse_id,
+				"external_id"=>$external_id,
+				"amount"=>$amount,
+				"bank_code"=>$bank_code,
+				"account_name"=>$account_name,
+				"account_number"=>$account_number,
+				"description"=>$description,
+				"status"=>"PENDING",
+				"created_datetime"=> $now,
+				"company_id"=>$company_id,
+				"user_id"=>$user_id
+			);
+			$sql = $this->db->insert("pouch_disbursements",$inserdata);
+			if($sql){
+				$response = array(
+					"id"=>md5($disburse_id),
+					"external_id"=>$external_id,
+					"amount"=>$amount,
+					"bank_code"=>$bank_code,
+					"account_name"=>$account_name,
+					"account_number"=>$account_number,
+					"description"=>$description,
+					"status"=>"PENDING",
+					"created_datetime"=> $now,
+					"user_id"=>md5($user_id)
+				);
+			}else{
+				$response = false;
+			}
+			return $response;
+		}
+		
+		function disburse_id($company_id){
+			$initiatx   = "TRDSB".$company_id;
+            $trnsc      = strlen($initiatx);
+            $month   = date("m");
+            $day     = date("d");
+            $year    = date("y");
+            $sql    = "SELECT left(a.disburse_id,2) as fmonth, mid(a.disburse_id,3,2) as fday," 
+                    . " mid(a.disburse_id,5,2) as fyear, mid(a.disburse_id,7,$trnsc) as initiat,"
+                    . " right(a.disburse_id,6) as fno FROM pouch_disbursements AS a"
+                    . " where left(a.disburse_id,2) = '$month' and mid(a.disburse_id,3,2) = '$day'"
+                    . " and mid(a.disburse_id,5,2) = '$year' and mid(a.disburse_id,7,$trnsc)= '$initiatx'"
+                    . " order by fmonth desc, CAST(fno AS SIGNED) DESC LIMIT 1";
+            // return $sql;
+            $result = $this->db->query($sql);	
+            
+            if($result->num_rows($result) > 0) {
+                $row = $result->row();
+                $initiat = $row->initiat;
+                $fyear = $row->fyear;
+                $fmonth = $row->fmonth;
+                $fday = $row->fday;
+                $fno = $row->fno;
+                $fno++;
+            } else {
+                $initiat = $initiatx;
+                $fyear   = $year;
+                $fmonth  = $month;
+                $fday    = $day;
+                $fno     = 0;
+                $fno++;
+            }
+            if (strlen($fno)==1){
+                $strfno = "00000".$fno;
+            } else if (strlen($fno)==2){
+                $strfno = "0000".$fno;
+            } else if (strlen($fno)==3){
+                $strfno = "000".$fno;
+            } else if (strlen($fno)==4){
+                $strfno = "00".$fno;
+            } else if (strlen($fno)==5){
+                $strfno = "0".$fno;
+            } else if (strlen($fno)==6){
+                $strfno = $fno;
+            }
+            
+            $disburse_id = $month.$day.$year.$initiat.$strfno;
+
+            return $disburse_id;
+		}
+	}
+?>
---
+++ application/modules/disbursement/controllers/Disbursement.php
@@ -0,0 +1,91 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require APPPATH . 'libraries/REST_Controller.php';
+class Disbursement extends REST_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->helper('url');
+
+		$this->load->view('welcome_message');
+	}
+	
+	function create_post(){
+		$this->load->model("ModelDisbursement");
+		// $headersa = apache_request_headers();
+		$headers=array();
+		foreach ($_SERVER as $name => $value) {
+			$headers[$name] = $value;
+		}
+		if(!isset($headers["HTTP_KEY"])){
+			$this->response([
+                'status' => FALSE,
+                'message' => 'INVALID_AUTHENTICATION'
+			], REST_Controller::HTTP_BAD_REQUEST);
+			return;
+		}
+
+		if($headers["HTTP_KEY"] == ""){
+			$this->response([
+				'status' => FALSE,
+				'message' => 'EMPTY_KEY'
+			], REST_Controller::HTTP_NOT_FOUND);
+			return;
+		}
+		$key  = $headers["HTTP_KEY"];
+		$tmpsecretkey = str_replace("pouch_dev_secret_","",$key);
+		$secret_key   = $this->aes->decrypt_aes256API($tmpsecretkey);
+		$arrsecretkey = explode("_",$secret_key);
+		if(!isset($arrsecretkey[1]) OR !isset($arrsecretkey[0])){
+			$this->response([
+				'status' => FALSE,
+				'message' => 'KEY_NOT_FOUND'
+			], REST_Controller::HTTP_NOT_FOUND);
+			return;
+		}
+		$user_id	  = $arrsecretkey[1];
+		$company_id	  = $arrsecretkey[0];
+		$cek_company_id = $this->ModelDisbursement->cek_company_id($company_id,$user_id);
+		if($cek_company_id != "registed"){
+			$this->response([
+				'status' => FALSE,
+				'message' => 'KEY_NOT_FOUND'
+			], REST_Controller::HTTP_NOT_FOUND);
+			return;
+		}
+		$external_id = $this->input->post("external_id");
+		$amount 	 = $this->input->post("amount");
+		$bank_code 	 = $this->input->post("bank_code");
+		$account_name 	= $this->input->post("account_holder_name");
+		$account_number = $this->input->post("account_number");
+		$description = $this->input->post("description");
+		$cek_amount = $this->ModelDisbursement->cek_amount($company_id,$amount);
+		if($cek_amount != "enough"){
+			$this->response([
+				'status' => FALSE,
+				'message' => 'OUT_OF_BALANCE'
+			], REST_Controller::HTTP_BAD_REQUEST);
+			return;
+		}
+
+		$data 		= array($external_id,$amount,$bank_code,$account_name,$account_number,$description,$user_id,$company_id);
+		$response	= $this->ModelDisbursement->create($data);
+		$this->response($response, REST_Controller::HTTP_OK);
+	}
+}
